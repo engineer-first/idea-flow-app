49427d7a9a8d9e9af7f7884f2e182769fa880031
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2026-07-06 14:51:06 +0900

    feature/44: 招待成功時のリダイレクトURLをエンコードした

--- app/invite/[inviteCode]/route.ts
+++ app/invite/[inviteCode]/route.ts
@@ -59,5 +59,5 @@ export async function GET(request: NextRequest, context: InviteRouteContext) {
     throw new Error(error.message);
   }
 
-  return redirectTo(request, `/rooms/${inviteCode}`);
+  return redirectTo(request, `/rooms/${encodeURIComponent(inviteCode)}`);
 }
